2575795baed6f7b8e97f626a78bfaf59e1e0f791
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-07-07 10:09:24 -0300

    Ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,61 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $estado = Estados::create([
-            'nome' => 'Ceará',
-        ]);
-        
-        $cidade = Cidades::create([
-            'nome' => 'Fortaleza',
-            'estado_id' => $estado->id
-        ]);
-
-        User::create([
-            'name' => 'Ramon',
-            'email' => 'ramon.ecotrackapp@gmail.com',
-            'password' => Hash::make('Senh@123'),
-            'role' => User::SUPERADMIN,
-        ]);
-
         User::create([
             'name' => 'Suporte Admin',
             'email' => 'admin@admin.com.br',
             'password' => Hash::make('Senh@123'),
             'role' => User::SUPERADMIN,
         ]);
-
-        // User::create([
-        //     'name' => 'Administrador',
-        //     'email' => 'administrador@administrador.com.br',
-        //     'password' => Hash::make('Senh@123'),
-        //     'role' => User::ADMINISTRADOR,
-        //     'cidade_id' => $cidade->id
-        // ]);
-
-        // User::create([
-        //     'name' => 'Operador',
-        //     'email' => 'operador@operador.com.br',
-        //     'password' => Hash::make('Senh@123'),
-        //     'role' => User::OPERADOR,
-        //     'cidade_id' => $cidade->id
-        // ]);
-
-        // User::create([
-        //     'name' => 'Motorista',
-        //     'email' => 'motorista@motorista.com.br',
-        //     'password' => Hash::make('Senh@123'),
-        //     'role' => User::MOTORISTA,
-        //     'cidade_id' => $cidade->id
-        // ]);
-
-        // User::create([
-        //     'name' => 'Publico',
-        //     'email' => 'publico@publico.com.br',
-        //     'password' => Hash::make('Senh@123'),
-        //     'role' => User::PUBLICO,
-        //     'cidade_id' => $cidade->id
-        // ]);
-
         
     }
 }
