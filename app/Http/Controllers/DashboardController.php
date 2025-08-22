2b974c7099e441b80818286e306bc0868f67709d
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-08-22 10:00:23 -0300

    Ajsutes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends Controller {
                 'data' => [
                     'total_veiculos' => Veiculo::get()->count(),
                     'total_veiculos_disponiveis' => Veiculo::get()->count(),
-                    'total_veiculos_uso' => Reserva::whereIsNull('data_hora_checkout')->get()->count(),
+                    'total_veiculos_uso' => Reserva::whereNull('data_hora_checkout')->get()->count(),
                     'total_veiculos_manutencao' => Reserva::whereIsNull('data_hora_checkout')->get()->count(),
                     'total_usuarios' => User::get()->count(),
                     'total_chamados' => Reserva::get()->count(),
