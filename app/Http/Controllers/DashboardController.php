2486f8338005e6147dd974d77b6d4d67e0ce0a1e
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-08-22 09:58:21 -0300

    Ajsutes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reserva;
+use App\Models\Veiculo;
+use App\User;
 use Exception;
 use Illuminate\Http\Request;
 class DashboardController extends Controller {
@@ -13,16 +16,18 @@ class DashboardController extends Controller {
     * @Route("/dashboard/totais")
     */
     public function totais(Request $request) {
-        try {    
+        try {
+
             $user = $request->user();
 
             return response()->json([
                 'data' => [
-                    'total_clientes' => 0,
-                    'total_usuarios' => 0,
-                    'total_carros' => 0,
-                    'total_horarios' => 0,
-                    'total_rotas' => 0,
+                    'total_veiculos' => Veiculo::get()->count(),
+                    'total_veiculos_disponiveis' => Veiculo::get()->count(),
+                    'total_veiculos_uso' => Reserva::whereIsNull('data_hora_checkout')->get()->count(),
+                    'total_veiculos_manutencao' => Reserva::whereIsNull('data_hora_checkout')->get()->count(),
+                    'total_usuarios' => User::get()->count(),
+                    'total_chamados' => Reserva::get()->count(),
                 ]
             ]);
             
