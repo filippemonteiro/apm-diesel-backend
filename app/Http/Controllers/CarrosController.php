7d0caa2e86fcae23410e58c96dbd26422fb5bf8a
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-10-26 22:05:44 -0300

    Configuracao Beberine

--- app/Http/Controllers/CarrosController.php
+++ app/Http/Controllers/CarrosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reserva;
 use App\Models\Veiculo;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,9 @@ class CarrosController extends Controller
     public function getCarroBYcode($codigo) {
         $codigo = substr($codigo, -1);
         $data = Veiculo::find($codigo);
+        if($data) {
+            $data->reserva = Reserva::where('veiculo_id', $data->id)->where('data_hora_checkout', null)->first();
+        }
         return response()->json($data);
     }
 }
